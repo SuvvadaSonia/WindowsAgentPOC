3c78909a84fae3dd0d044fd6be1d1be478ac0cea
SuvvadaSonia <suvvadasonia@gmail.com>
2022-11-28 09:31:14 +0530

    final commit

--- run.py
+++ run.py
@@ -4,33 +4,27 @@
     
 """
 import paramiko
+# Paramiko is a Python library that makes a connection with a remote device through SSh
 
-
-class RemoteAgent:
-    def __init__(self, hostname, username, password) -> None:
-        self.hostname = hostname
-        self.username = username
-        self.password = password
-        
+class windowsAgent:
     
-    def connect_to_remote_host(self,client):  # It establishes connection with remote device 
+    def __init__(self, hostname: str, auth:tuple) -> None:
+        self.__user, self.__pwd = auth
+        self.hostname = hostname
+
+
+    def getConfig(self,client):             # connects to ubuntu machine using paramiko 
         client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        client.connect(hostname= self.hostname, username= self.username, password= self.password)
-        
-        
-    def execute_commands(self,client):   # It executes commands in remote device and prints response on local device
+        client.connect(hostname= self.hostname, username= self.__user, password= self.__pwd)
         commands = ['df -H','uname -a','hostname -i']
 
         for command in commands:
             stdin, stdout, stderr = client.exec_command(command)
             print(stdout.read().decode())
 
-    def close_connection(self,client):  # It closes the established connection with remote device 
-        client.close()
 
 if __name__ == "__main__":
     client = paramiko.SSHClient()
-    connection = RemoteAgent('192.168.1.237','root','India@123')  
-    connection.connect_to_remote_host(client)
-    connection.execute_commands(client)
-    connection.close_connection(client)
+    connection = windowsAgent('192.168.1.237',('root','India@123'))
+    connection.getConfig(client)
+
